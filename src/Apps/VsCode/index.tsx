a27e0f46b08fcd076f40889c211d5c7db9fc7704
Descendo The Monkei <61548447+hayden-droid@users.noreply.github.com>
2022-11-21 12:52:12 +0000

    Update index.tsx

--- src/Apps/VsCode/index.tsx
+++ src/Apps/VsCode/index.tsx
@@ -1,7 +1,7 @@
 export const VsCodeApp = () => {
   return (
     <iframe
-      src="https://www.tiktok.com/@the.new.memeitizer/video/7139286635664936197?is_from_webapp=1&sender_device=pc&web_id=7168445371277526534"
+      src="https://hayden-droid.github.io/testttt/"
       title="Memeitizer On TikTok"
       width="100%"
       height="100%"
